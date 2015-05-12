4b97a64a4394149c4079f5a2fa1695e6f1ecdec4
Admin <admin@Mac-Admin.local>
2015-05-12 06:40:28 +0300

    add custom cell in project

---
+++ Parser/CustomCell.h
@@ -0,0 +1,16 @@
+//
+//  CustomCell.h
+//  Parser
+//
+//  Created by Admin on 07.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageBlock;
+@property (weak, nonatomic) IBOutlet UILabel *mainLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subLabel;
+
+@end
---
+++ Parser/CustomCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomCell.m
+//  Parser
+//
+//  Created by Admin on 07.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import "CustomCell.h"
+
+@implementation CustomCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
